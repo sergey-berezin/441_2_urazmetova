3b01526aa93b28d6d4b979773c37476f2635ea15
marritau <104126121+marritau@users.noreply.github.com>
2024-11-24 23:21:46 +0300

    task 3

---
+++ lab3/ClassLibrary1/Route.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Route
+    {
+        public List<int> Cities { get; private set; }
+        private double[,] distanceMatrix;
+
+        public Route(double[,] distanceMatrix)
+        {
+            Cities = Enumerable.Range(0, distanceMatrix.GetLength(0)).ToList();
+            this.distanceMatrix = distanceMatrix;
+            Shuffle();
+        }
+
+        public Route(List<int> cities, double[,] distanceMatrix)
+        {
+            Cities = cities;
+            this.distanceMatrix = distanceMatrix;
+        }
+
+        public Route()
+        {
+            Cities = new List<int>();
+            distanceMatrix = new double[0, 0];
+        }
+
+        public double Length
+        {
+            get
+            {
+                double length = 0;
+                for (int i = 0; i < Cities.Count; i++)
+                {
+                    int nextCity = (i + 1) % Cities.Count;
+                    length += distanceMatrix[Cities[i], Cities[nextCity]];
+                }
+                return length;
+            }
+        }
+
+        private void Shuffle()
+        {
+            Random rand = new Random();
+            for (int i = Cities.Count - 1; i > 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                (Cities[i], Cities[j]) = (Cities[j], Cities[i]);
+            }
+        }
+
+        public void Mutate()
+        {
+            Random rand = new Random();
+            int i = rand.Next(Cities.Count);
+            int j = rand.Next(Cities.Count);
+            (Cities[i], Cities[j]) = (Cities[j], Cities[i]);
+        }
+    }
+}
---
+++ lab3/WpfApp1_M/Manager.cs
@@ -0,0 +1,85 @@
+﻿using ClassLibrary1;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WpfApp1_M
+{
+    public class Manager
+    {
+        private const string ExperimentsFile = "runs.json";
+        private const string PopulationDirectory = "PopulationData";
+
+        public Manager()
+        {
+            if (!Directory.Exists(PopulationDirectory))
+            {
+                Directory.CreateDirectory(PopulationDirectory);
+            }
+        }
+
+        public List<Experiment> LoadExperiments()
+        {
+            if (File.Exists(ExperimentsFile))
+            {
+                string json = File.ReadAllText(ExperimentsFile);
+                return JsonConvert.DeserializeObject<List<Experiment>>(json) ?? new List<Experiment>();
+            }
+            return new List<Experiment>();
+        }
+
+        public void SaveExperiment(string experimentName, List<Route> population)
+        {
+            string populationFile = Path.Combine(PopulationDirectory, $"{experimentName}_population.json");
+
+            string populationJson = JsonConvert.SerializeObject(population, Formatting.Indented);
+            File.WriteAllText(populationFile, populationJson);
+
+            List<Experiment> experiments = LoadExperiments();
+            var existingExperiment = experiments.FirstOrDefault(e => e.Name == experimentName);
+            if (existingExperiment == null)
+            {
+                experiments.Add(new Experiment
+                {
+                    Name = experimentName,
+                    PopulationFileName = populationFile
+                });
+
+                string experimentsJson = JsonConvert.SerializeObject(experiments, Formatting.Indented);
+                File.WriteAllText(ExperimentsFile, experimentsJson);
+            }
+        }
+        
+        public void LoadExperimentPopulation(string experimentName, GeneticAlgorithm ga)
+        {
+            List<Experiment> experiments = LoadExperiments();
+            var experiment = experiments.FirstOrDefault(e => e.Name == experimentName);
+
+            if (experiment == null)
+            {
+                throw new Exception("Эксперимент не найден.");
+            }
+
+            string populationFile = experiment.PopulationFileName;
+
+            if (File.Exists(populationFile))
+            {
+                string populationJson = File.ReadAllText(populationFile);
+                var population = JsonConvert.DeserializeObject<List<Route>>(populationJson);
+                ga.SetPopulation(population);
+            }
+            else
+            {
+                throw new Exception("Популяция для эксперимента не найдена.");
+            }
+        }
+    }
+
+    public class Experiment
+    {
+        public string Name { get; set; }
+        public string PopulationFileName { get; set; }
+    }
+}
